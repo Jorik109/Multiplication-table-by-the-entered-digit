c6ac5d7f87b5e27a726ee6843326a4fa7d807fe9
David Kotov <David Kotov@DESKTOP-3U9FBHN>
2023-03-13 21:36:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+            int inputInt = Convert.ToInt32(inputString);
+            string inputString2 = "*";
+            string inputString4 = "=";
+            string inputString3 = "1";
+
+            if (1 == inputInt) 
+            {
+                for (int i = 1; i < 10; i++)
+
+                {
+                    MethodJorka32(inputString3 + inputString2 + i + inputString4 + 1*i);
+
+                }
+            }
+            else
+            {
+                
+                string inputString33 = "2";
+
+                if (2 == inputInt)
+                {
+                    for (int i = 1; i < 10; i++)
+
+                    {
+                        MethodJorka32(inputString33 + inputString2 + i + inputString4 + 2 * i);
+
+                    }
+                }
+                else
+                {
+                    
+                    string inputString333 = "3";
+
+                    if (3 == inputInt)
+                    {
+                        for (int i = 1; i < 10; i++)
+
+                        {
+                            MethodJorka32(inputString333 + inputString2 + i + inputString4 + 3 * i);
+
+                        }
+                    }
+                    else
+                    {
+                        
+                        string inputString3333 = "4";
+
+                        if (4 == inputInt)
+                        {
+                            for (int i = 1; i < 10; i++)
+
+                            {
+                                MethodJorka32(inputString3333 + inputString2 + i + inputString4 + 4 * i);
+
+                            }
+                        }
+                        else
+                        {
+                            string inputString3334 = "5";
+
+                            if (5 == inputInt)
+                            {
+                                for (int i = 1; i < 10; i++)
+
+                                {
+                                    MethodJorka32(inputString3334 + inputString2 + i + inputString4 + 5 * i);
+
+                                }
+                            }
+                            else
+                            {
+                                string inputString3335 = "6";
+
+                                if (6 == inputInt)
+                                {
+                                    for (int i = 1; i < 10; i++)
+
+                                    {
+                                        MethodJorka32(inputString3335 + inputString2 + i + inputString4 + 6 * i);
+
+                                    }
+                                }
+                                else
+                                {
+                                    string inputString3336 = "7";
+
+                                    if (7 == inputInt)
+                                    {
+                                        for (int i = 1; i < 10; i++)
+
+                                        {
+                                            MethodJorka32(inputString3336 + inputString2 + i + inputString4 + 7 * i);
+
+                                        }
+                                    }
+                                    else
+                                    {
+                                        string inputString3337 = "8";
+
+                                        if (8 == inputInt)
+                                        {
+                                            for (int i = 1; i < 10; i++)
+
+                                            {
+                                                MethodJorka32(inputString3337 + inputString2 + i + inputString4 + 8 * i);
+
+                                            }
+                                        }
+                                        else
+                                        {
+                                            string inputString3338 = "9";
+
+                                            if (9 == inputInt)
+                                            {
+                                                for (int i = 1; i < 10; i++)
+
+                                                {
+                                                    MethodJorka32(inputString3338 + inputString2 + i + inputString4 + 9 * i);
+
+                                                }
+                                            }
+                                           
+                                            {
+
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }   
+                    
+            }
+            
+        }
+
+        public static void MethodJorka32(string inputStringReady)
+        {
+            Console.WriteLine(inputStringReady);
+
+        }
+    }
+
+}
